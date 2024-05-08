a633d2ebb68ed2a77994f32c2b20cc8984a8d269
e.nazyrov <collloh69@gmail.com>
2024-05-09 00:14:46 +0300

    Удалены отзывы

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -7,18 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.example.model.Review;
-import com.example.service.ReviewService;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 @RequestMapping
 public class MainController {
-
-    @Autowired
-    private ReviewService reviewService;
-
     @Autowired
     private PerformService performService;
 
@@ -44,18 +37,6 @@ public class MainController {
     @RequestMapping(path = "/profile")
     public String profilePage(Model model) {
         return "profile";
-    }
-
-    @RequestMapping(path = "/feedback")
-    public String reviewsPage(Model model) {
-        model.addAttribute("reviews", reviewService.getAll());
-        return "reviews";
-    }
-
-    @RequestMapping(path = "/feedback/editor/submit", method = RequestMethod.POST)
-    public String submitReview(@ModelAttribute Review review) {
-        reviewService.save(review);
-        return "redirect:../";
     }
 
     @RequestMapping(path = "/login")
